201d490a02c729a3216db7e6e4252c16634ef657
Selom Amuzu <selomka1@gmail.com>
2025-10-13 17:53:32 +0000

    Removed built js files

--- src/routes/clerkWebhookRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var clerkWebhook_controller_1 = require("../controllers/clerkWebhook.controller");
-var clerkRouter = express_1.default.Router();
-clerkRouter.post("/users", express_1.default.raw({ type: 'application/json' }), clerkWebhook_controller_1.ClerkWebhookController.UserCreateWebhook);
-exports.default = clerkRouter;
